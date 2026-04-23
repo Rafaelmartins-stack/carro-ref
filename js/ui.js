c406d0eb605bd3943bbe89e72b4b1bb7dfd348b7
Rafaelmartins-stack <familiarafael00@gmail.com>
2026-04-23 14:40:37 -0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -18,6 +18,11 @@ const UI = {
             document.getElementById('nitro-2'),
             document.getElementById('nitro-3')
         ];
+
+        this.mapCanvas = document.getElementById('mapCanvas');
+        this.mapCtx = this.mapCanvas.getContext('2d');
+        this.mapCanvas.width = 100;
+        this.mapCanvas.height = 150;
     },
 
     update() {
@@ -47,5 +52,28 @@ const UI = {
                 this.elements.nitros[i].classList.remove('used');
             }
         }
-    }
+
+        this.updateMap();
+    },
+
+    updateMap() {
+        const ctx = this.mapCtx;
+        ctx.clearRect(0, 0, 100, 150);
+
+        // Simple vertical progress line
+        ctx.strokeStyle = '#555';
+        ctx.lineWidth = 4;
+        ctx.beginPath();
+        ctx.moveTo(50, 10);
+        ctx.lineTo(50, 140);
+        ctx.stroke();
+
+        // Player position
+        const progress = Player.z / Track.trackLength;
+        const y = 140 - (progress * 130);
+        ctx.fillStyle = 'var(--primary)';
+        ctx.beginPath();
+        ctx.arc(50, y, 5, 0, Math.PI * 2);
+        ctx.fill();
+    },
 };
